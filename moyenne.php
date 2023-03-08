7f1a068806bb635f0410ddc6be39a5a5b6c20b67
unknown <nicolassam33@gmail.com>
2023-03-08 11:22:56 +0100

    modification moyen in float

--- moyenne.php
+++ moyenne.php
@@ -1,10 +1,10 @@
 <?php
 
-function moyenne(int $php,int $js,int $css): int{
+function moyenne(float $php,float $js,float $css): float{
     return  ($php + $css + $js)/3;
 }
 
-    echo "La moyenne des 3 matières est de : " . moyenne(5,10,5);
+echo "La moyenne des 3 matières est de : " . moyenne(5.5,10.4,5.4);
 
 
 
